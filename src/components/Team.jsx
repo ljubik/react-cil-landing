9a38ea239a13053b9055f77a9d4478fc376ae520
Liubomyr <ljubik@ukr.net>
2021-08-07 20:02:48 +0300

    prepare to work

---
+++ src/components/Team.jsx
@@ -0,0 +1,34 @@
+import  ButtonMailto  from "./Mailtobtn/Mailtobtn";
+
+export const Team = (props) => {
+  return (
+    <div id='team' className='text-center'>
+      <div className='container'>
+        <div className='col-md-8 col-md-offset-2 section-title'>
+          <h2>Наша команда</h2>
+          <p>
+            Ми знаходимось на шляху від потреби до її вирішення.
+          </p>
+        </div>
+        <div id='row'>
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.name}-${i}`} className='col-md-3 col-sm-6 team'>
+                  <div className='thumbnail'>
+                    {' '}
+                    <img src={d.img} alt="Фото {d.name}" className='team-img' />
+                    <div className='caption'>
+                      <h4>{d.name}</h4>
+                      <p>{d.job}</p>
+                      <p>{<a href="mailto:{d.contact}">{d.contact}</a>}</p>
+                      <ButtonMailto label="Write me an E-Mail" mailto={d.contact} />
+                    </div>
+                  </div>
+                </div>
+              ))
+            : 'loading'}
+        </div>
+      </div>
+    </div>
+  )
+}
